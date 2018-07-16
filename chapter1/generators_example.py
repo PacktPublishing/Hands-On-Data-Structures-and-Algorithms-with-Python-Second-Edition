95868eacc5d94ad04994914e51d1e0b9757ae96a
Basant Agarwal <thebasant@gmail.com>
2018-07-16 15:25:19 +0530

    Update generators_example.py

--- chapter1/generators_example.py
+++ chapter1/generators_example.py
@@ -1,17 +1,21 @@
-#compares the running time of a list compared to a generator  import time
+#compares the running time of a list compared to a generator  
+
+import time
+
 #generator function creates an iterator of odd numbers between n and m 
 
 def oddGen(n,m):
-	whilen<m:
-	yield n 
-	n += 2
+	while n<m:
+		yield n 
+		n += 2
 
 #builds a list of odd numbers between n and m 
 
 def oddLst(n,m):
 	lst=[]
 	while n<m: 
-		lst.append(n) n+=2
+		lst.append(n) 
+		n+=2
 	return lst
 
 #the time it takes to perform sum on an iterator 
