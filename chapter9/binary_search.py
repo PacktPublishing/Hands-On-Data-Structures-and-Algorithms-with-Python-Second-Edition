26509e49021e52f88bc87441c71981d160813538
Basant Agarwal <thebasant@gmail.com>
2018-09-03 23:54:01 +0530

    Create binary_search.py

---
+++ chapter9/binary_search.py
@@ -0,0 +1,19 @@
+
+
+def binary_search(ordered_list, term):
+    size_of_list = len(ordered_list) - 1
+    index_of_first_element = 0
+    index_of_last_element = size_of_list
+    while index_of_first_element <= index_of_last_element:
+        mid_point = (index_of_first_element + index_of_last_element)//2
+        if ordered_list[mid_point] == term:
+            return mid_point
+        if term > ordered_list[mid_point]:
+            index_of_first_element = mid_point + 1
+        else:
+            index_of_last_element = mid_point - 1
+
+
+store = [2, 4, 5, 12, 43, 54, 60, 77]
+a = binary_search(store, 2)
+print("Index position of value 2 is ",a)
